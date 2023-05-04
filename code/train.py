bfaf594b52a92821113a41902286c048e585962b
GrapeDiget <alsgh8257@naver.com>
2023-05-04 10:58:23 +0000

    initial commit

---
+++ code/train.py
@@ -0,0 +1,98 @@
+import sys
+sys.path.append("../utils")
+
+from metric import *
+from preprocessing import *
+from tokenizing import *
+from load_data import *
+from transformers import AutoTokenizer, AutoConfig, AutoModelForSequenceClassification, Trainer, TrainingArguments, RobertaConfig, RobertaTokenizer, RobertaForSequenceClassification, BertTokenizer
+import torch
+import pickle as pickle
+
+
+
+def label_to_num(label):
+    num_label = []
+    with open('dict_label_to_num.pkl', 'rb') as f:
+        dict_label_to_num = pickle.load(f)
+    for v in label:
+        num_label.append(dict_label_to_num[v])
+
+    return num_label
+
+
+def train():
+    # load model and tokenizer
+    # MODEL_NAME = "bert-base-uncased"
+    MODEL_NAME = "klue/bert-base"
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+
+    # load dataset
+    train_dataset = load_data("../data/train/train.csv")
+    # dev_dataset = load_data("../data/train/dev.csv") # validation용 데이터는 따로 만드셔야 합니다.
+
+    train_label = label_to_num(train_dataset['label'].values)
+    # dev_label = label_to_num(dev_dataset['label'].values)
+
+    # tokenizing dataset
+    tokenized_train = tokenized_dataset(train_dataset, tokenizer)
+    # tokenized_dev = tokenized_dataset(dev_dataset, tokenizer)
+
+    # make dataset for pytorch.
+    RE_train_dataset = RE_Dataset(tokenized_train, train_label)
+    # RE_dev_dataset = RE_Dataset(tokenized_dev, dev_label)
+
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    print(device)
+    # setting model hyperparameter
+    model_config = AutoConfig.from_pretrained(MODEL_NAME)
+    model_config.num_labels = 30
+
+    model = AutoModelForSequenceClassification.from_pretrained(
+        MODEL_NAME, config=model_config)
+    print(model.config)
+    model.parameters
+    model.to(device)
+
+    # 사용한 option 외에도 다양한 option들이 있습니다.
+    # https://huggingface.co/transformers/main_classes/trainer.html#trainingarguments 참고해주세요.
+    training_args = TrainingArguments(
+        output_dir='./results',          # output directory
+        save_total_limit=5,              # number of total save model.
+        save_steps=500,                 # model saving step.
+        num_train_epochs=20,              # total number of training epochs
+        learning_rate=5e-5,               # learning_rate
+        per_device_train_batch_size=16,  # batch size per device during training
+        per_device_eval_batch_size=16,   # batch size for evaluation
+        warmup_steps=500,                # number of warmup steps for learning rate scheduler
+        weight_decay=0.01,               # strength of weight decay
+        logging_dir='./logs',            # directory for storing logs
+        logging_steps=100,              # log saving step.
+        evaluation_strategy='steps',  # evaluation strategy to adopt during training
+        # `no`: No evaluation during training.
+        # `steps`: Evaluate every `eval_steps`.
+        # `epoch`: Evaluate every end of epoch.
+        eval_steps=500,            # evaluation step.
+        load_best_model_at_end=True
+    )
+    trainer = Trainer(
+        # the instantiated 🤗 Transformers model to be trained
+        model=model,
+        args=training_args,                  # training arguments, defined above
+        train_dataset=RE_train_dataset,         # training dataset
+        eval_dataset=RE_train_dataset,             # evaluation dataset
+        compute_metrics=compute_metrics         # define metrics function
+    )
+
+    # train model
+    trainer.train()
+    model.save_pretrained('./best_model')
+
+
+def main():
+    train()
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils/tokenizing.py
@@ -0,0 +1,17 @@
+def tokenized_dataset(dataset, tokenizer):
+    """ tokenizer에 따라 sentence를 tokenizing 합니다."""
+    concat_entity = []
+    for e01, e02 in zip(dataset['subject_entity'], dataset['object_entity']):
+        temp = ''
+        temp = e01 + '[SEP]' + e02
+        concat_entity.append(temp)
+    tokenized_sentences = tokenizer(
+        concat_entity,
+        list(dataset['sentence']),
+        return_tensors="pt",
+        padding=True,
+        truncation=True,
+        max_length=256,
+        add_special_tokens=True,
+    )
+    return tokenized_sentences

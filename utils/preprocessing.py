d575db18f5b7062c6233b61cb36daaa06ef9e906
jihyeeon <zizi39028@gmail.com>
2023-05-11 10:33:03 +0000

    fix: function name

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -37,7 +37,7 @@ def punc_preprocessing_dataset(dataset):
                                'subject_entity': subject_entity, 'object_entity': object_entity, 'label': dataset['label'], })
     return out_dataset
 
-def punc_type_preprocessing_dataset(dataset):
+def punc_typed_preprocessing_dataset(dataset):
     """ 처음 불러온 csv 파일을 원하는 형태의 DataFrame으로 변경 시켜줍니다."""
     subject_entity = []
     object_entity = []
@@ -63,6 +63,6 @@ def load_data(dataset_dir):
     pd_dataset = pd.read_csv(dataset_dir)
     #dataset = preprocessing_dataset(pd_dataset)
     #dataset = punc_preprocessing_dataset(pd_dataset)
-    dataset = punc_type_preprocessing_dataset(pd_dataset)
+    dataset = punc_typed_preprocessing_dataset(pd_dataset)
 
     return dataset

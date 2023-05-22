e4cec95984fe778ab8dcf63543557f941d315573
Jihyeon <50359820+jihyeeon@users.noreply.github.com>
2023-05-22 23:22:17 +0900

    Fix: load_data.py

--- utils/load_data.py
+++ utils/load_data.py
@@ -5,21 +5,14 @@ import torch
 class RE_Dataset(torch.utils.data.Dataset):
     """ Dataset 구성을 위한 class."""
 
-    def __init__(self, pair_dataset, RE_labels, S_labels, O_labels):
+    def __init__(self, pair_dataset, labels):
         self.pair_dataset = pair_dataset
-        self.labels = RE_labels
-        self.s_labels = S_labels
-        self.o_labels = O_labels
+        self.labels = labels
 
     def __getitem__(self, idx):
         item = {key: val[idx].clone().detach()
                 for key, val in self.pair_dataset.items()}
-        if self.s_labels:
-            item['labels'] = torch.tensor([self.labels[idx], self.s_labels[idx],self.s_labels[idx]])
-        else:
-            item['labels'] = torch.tensor(self.labels[idx])
-        #item['s_labels']= torch.tensor(self.s_labels[idx])
-        #item['o_labels']= torch.tensor(self.s_labels[idx])
+        item['labels'] = torch.tensor(self.labels[idx])
         return item
 
     def __len__(self):

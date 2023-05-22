6c294e2c90aa33f2e19cbd01f2c81b9484cd1968
김성은_T5036 <sekim520@gmail.com>
2023-05-22 17:06:51 +0900

    Fix: merge Submain
    - swap entity preprocessing 함수를 Submain에 추가하기 위한 merge

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -95,22 +95,7 @@ def load_data(dataset_dir, ppc_mode):
     # dataset을 사용하여 원하는 preprocessing 함수를 적용할 수 있습니다.
     ### config 파일에 원하는 preprocessing 함수를 나열 해 주세요 ###
     # DA/ DC 적용 코드
-    print(ppc_mode)
-
-    swap = swap_entities(dataset)
-    dataset = pd.concat([dataset, swap])
 
     ### config 파일에 원하는 preprocessing_dataset 함수를 입력 해 주세요 ###
     dataset = globals()[ppc_mode](dataset)
-    
     return dataset
-
-if __name__ == "__main__":
-    path = "./data/train/dev.csv"
-
-    with open('./config.yaml') as f:
-        CFG = yaml.load(f, Loader=yaml.FullLoader)
-    prep = CFG['data']['preprocessing']
-    print(prep)
-
-    data = load_data(path, prep)

f0290d9930b9eceb2451a66e637e8dfc7a41151b
Jihyeon <50359820+jihyeeon@users.noreply.github.com>
2023-05-22 23:23:04 +0900

    Fix: metric.py

--- utils/metric.py
+++ utils/metric.py
@@ -38,7 +38,7 @@ def klue_re_auprc(probs, labels):
 
 def compute_metrics(pred):
     """ validation을 위한 metrics function """
-    labels = pred.label_ids[:,0]
+    labels = pred.label_ids
     preds = pred.predictions.argmax(-1)
     probs = pred.predictions
 

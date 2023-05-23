402a30975c77893c800aaa179035ad65d417fc9d
김성은_T5036 <sekim520@gmail.com>
2023-05-23 13:19:20 +0900

    Fix: tokenizing
    Submain과 동일한 버전으로 통일함

--- utils/tokenizing.py
+++ utils/tokenizing.py
@@ -48,53 +48,6 @@ def tokenized_dataset_with_wordtype(df, tokenizer, max_len):
               '[ODAT]','[/ODAT]','[OLOC]','[/OLOC]',
               '[OPOH]','[/OPOH]','[ONOH]','[/ONOH]']
     
-    tokenizer.add_tokens(tokens, special_tokens=True)
-    # tokenizer.additional_special_tokens(tokens)
-    
-    tokenized_sentences = tokenizer(
-        sents,
-        return_tensors="pt",
-        padding=True,
-        truncation=True,
-        max_length=max_len,
-        add_special_tokens=True,
-        # return_special_tokens_mask=True
-    )
-    # breakpoint()
-    
-    return tokenized_sentences
-
-
-def only_wordtype(df, tokenizer, max_len):
-    """ tokenizer에 따라 sentence를 tokenizing 합니다."""
-    sents = []
-    
-    for idx, row in df.iterrows():
-        sent = ''
-        s_st = row['subject_begin']
-        s_end = row['subject_end']
-        o_st = row['object_begin']
-        o_end = row['object_end']
-    
-        if s_st < o_st:
-            sent = row['sentence']
-            sent = sent[:s_st] + '[' +row['subject_entity_type'] + ']' + sent[s_st:]
-            sent = sent[:s_end+7] + '[/' +row['subject_entity_type'] + ']' + sent[s_end+7:]
-            sent = sent[:o_st+13] + '[' +row['object_entity_type'] + ']' + sent[o_st+13:]
-            sent = sent[:o_end+20] + '[/' +row['object_entity_type'] + ']' + sent[o_end+20:]
-        else :
-            sent = row['sentence']
-            sent = sent[:o_st] + '[' +row['object_entity_type'] + ']' + sent[o_st:]
-            sent = sent[:o_end+7] + '[' +row['object_entity_type'] + ']' + sent[o_end+7:]
-            sent = sent[:s_st+13] + '[' +row['subject_entity_type'] + ']' + sent[s_st+13:]
-            sent = sent[:s_end+20] + '[/' +row['subject_entity_type'] + ']' + sent[s_end+20:]
-        
-        sents.append(sent)
-    
-    tokens = ['[PER]','[/PER]','[ORG]','[/ORG]',
-              '[DAT]','[/DAT]','[LOC]','[/LOC]',
-              '[POH]','[/POH]','[NOH]','[/NOH]']
-    
     tokenizer.add_tokens(tokens,special_tokens=True)
     
     tokenized_sentences = tokenizer(
@@ -107,46 +60,3 @@ def only_wordtype(df, tokenizer, max_len):
     )
     
     return tokenized_sentences
-
-def entity_tokens(df, tokenizer, max_len):
-    """
-    entity 단어 앞, 뒤에 entity start, end token을 subject, object를 구분하여 넣습니다. 
-    Ex) 이순신은 조선 중기의 무신이다. -> [SUB] 이순신 [\SUB] 은 [OBJ] 조선 [\OBJ] 중기의 무신이다. 
-    """
-    sentences = []
-    for id, item in df.iterrows():
-        if item['subject_begin'] < item['object_begin']:
-            sent = item['sentence']
-            sent = sent[:item['subject_begin']] + '[SUB]' + sent[item['subject_begin']:]
-            sent = sent[:item['subject_end']+6] + '[/SUB]' + sent[item['subject_end']+6:]
-            sent = sent[:item['object_begin']+11] + '[OBJ]' + sent[item['object_begin']+11:]
-            sent = sent[:item['object_end']+17] + '[/OBJ]' + sent[item['object_end']+17:]
-        else :
-            sent = item['sentence']
-            sent = sent[:item['object_begin']] + '[OBJ]' + sent[item['object_begin']:]
-            sent = sent[:item['object_end']+6] + '[/OBJ]' + sent[item['object_end']+6:]
-            sent = sent[:item['subject_begin']+11] + '[SUB]' + sent[item['subject_begin']+11:]
-            sent = sent[:item['subject_end']+17] + '[/SUB]' + sent[item['subject_end']+17:]
-        sentences.append(sent)
-
-    tokens = ['[SUB]', '[/SUB]', '[OBJ]', '[/OBJ]']
-    tokenizer.add_tokens(tokens, special_tokens=True)
-
-    tokenized_sentences = tokenizer(
-        sentences,
-        return_tensors="pt",
-        padding=True,
-        truncation=True,
-        max_length=max_len,
-        add_special_tokens=True
-    )
-    # breakpoint()
-    subject_token = '[SUB]'
-    object_token = '[OBJ]'
-    tokens_index = []
-    for i in range(len(tokenized_sentences)):
-        tokens = tokenized_sentences[i].tokens
-        idx = [0, tokens.index(subject_token), tokens.index(object_token)]
-        tokens_index.append(idx)
-    # breakpoint()
-    return tokenized_sentences, tokens_index

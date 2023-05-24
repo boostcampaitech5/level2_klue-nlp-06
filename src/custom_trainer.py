af2c4286bc2f7efba41e8bd3a97947685ce2beb2
김성은_T5036 <sekim520@gmail.com>
2023-05-24 10:26:46 +0900

    Fix: merge Submain
    - custom trainer에서 loss 함수 삭제

--- src/custom_trainer.py
+++ src/custom_trainer.py
@@ -33,32 +33,3 @@ class CustomTrainer(Trainer):
 
             return self.lr_scheduler
 
-        def compute_loss(self, model, inputs, return_outputs=False):
-            """
-            loss 함수를 커스텀할 수 있습니다. 
-            """
-            if self.label_smoother is not None and "labels" in inputs:
-                labels = inputs.pop("labels")
-            else:
-                labels = None
-            outputs = model(**inputs)
-            # Save past state if it exists
-            # TODO: this needs to be fixed and made cleaner later.
-            if self.args.past_index >= 0:
-                self._past = outputs[self.args.past_index]
-
-            if labels is not None:
-                if unwrap_model(model)._get_name() in MODEL_FOR_CAUSAL_LM_MAPPING_NAMES.values():
-                    loss = self.label_smoother(outputs, labels, shift_labels=True)
-                else:
-                    loss = self.label_smoother(outputs, labels)
-            else:
-                if isinstance(outputs, dict) and "loss" not in outputs:
-                    raise ValueError(
-                        "The model did not return a loss from the inputs, only the following keys: "
-                        f"{','.join(outputs.keys())}. For reference, the inputs it received are {','.join(inputs.keys())}."
-                    )
-                # We don't use .loss here since the model may return tuples instead of ModelOutput.
-                loss = outputs["loss"] if isinstance(outputs, dict) else outputs[0]
-
-            return (loss, outputs) if return_outputs else loss
